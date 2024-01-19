21d2e498e8ff5a19072d1d27581fd7fc4a1a52fe
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-19 16:55:38 +0100

    Add todo functionality and update moody.py

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
+import os
 todo = []
+filename = "todo.txt"
+
+if not os.path.exists(filename):
+    with open(filename, "w") as file:
+        pass
+
+with open(filename, "r") as file:
+    todo = file.read().splitlines()
 
 while True:
     user_action = input("Type what you want to do: Add, Show, Edit, Remove, q/quit ").strip().lower()
     match user_action:
         case "add":
-            todo.append(input("Type your todo: "))
+            new_todo = input("Type your todo: ")
+            todo.append(new_todo)
+            with open(filename, "a") as file:
+                file.write(f"{new_todo}\n")
         case "show":
             for item in todo:
                 item = item.title()
-                print(item)
+                print(f"Todo: {item}")
         case "edit":
+            print("Current items:")
+            for i, item in enumerate(todo):
+                print(f"{i}: {item}")
             number = int(input("Type the number of the item you want to edit: "))
-            todo[number] = input("Type your new todo: ")
-            print("Item edited", todo)
+            previous_todo = todo[number]
+            new_todo = input("Type your new todo: ")
+            todo[number] = new_todo
+            print(f"Item {previous_todo} edited. New todo: {new_todo}")
+            with open(filename, "w") as file:
+                file.write("\n".join(todo))
         case "remove":
             number = int(input("Type the number of the item you want to remove: "))
             del todo[number]
-            print("Item removed", todo)
+            print(f"Item {number} removed.")
+            with open(filename, "w") as file:
+                file.write("\n".join(todo))
+        case "complete":
+            print("Current items:")
+            for i, item in enumerate(todo):
+                print(f"{i}: {item}")
+            number = int(input("Type the number of the item you want to complete: "))
+            completed_todo = todo[number]
+            del todo[number]
+            print(f"Item {completed_todo} completed and removed.")
+            with open(filename, "w") as file:
+                file.write("\n".join(todo))
         case _:
             if user_action in ['q', 'quit']:
                 print("Goodbye!")
--- test/moody.py
+++ test/moody.py
@@ -5,3 +5,22 @@ color_codes = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
 
 serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
 print(serials[2])
+
+seconds = [1.23, 1.45, 1.02]
+current = 1.11
+seconds.append(current)
+
+filenames = ['Document.txt', 'Report.txt', 'Presentation.txt']
+for i, filename in enumerate(filenames):
+    print(f"{i}-{filename}")
+    
+ips = ['100.122.133.105', '100.122.133.111']
+input_nr = int(input("Type the number of the IP you want to see: "))
+print(f"You chose {ips[input_nr]}")
+
+temperatures = [float(3.7), int(4.5), "hello"]
+
+rainfall = [3.7, 4, 'hello', ['hello']]
+
+seconds = [1.23, 1.45, 1.02, 1.11]
+seconds.pop(1)
