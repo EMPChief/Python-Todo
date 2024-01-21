4af6ef457702542d9c196fcc15f987b593fef081
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-21 11:44:12 +0100

    Remove unused code and deleted tasks.txt file

--- utility/FileOperations.py
+++ utility/FileOperations.py
@@ -2,15 +2,12 @@ import json
 from pathlib import Path
 
 class FileOperations:
-    # Set the filename relative to the Python-Todo directory
     filename = Path("db/db.json")
 
     @staticmethod
     def initialize_tasks():
         if not FileOperations.filename.exists():
-            # Create the db directory if it doesn't exist
             FileOperations.filename.parent.mkdir(parents=True, exist_ok=True)
-            # Create an empty JSON file
             with FileOperations.filename.open("w") as file:
                 json.dump([], file)
             return []
